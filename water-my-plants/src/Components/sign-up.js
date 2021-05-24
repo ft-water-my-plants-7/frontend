d42eef035cdbe1f3f54d627f54591db19bb8aab5
adampatt <adampattison08@gmail.com>
2021-05-24 22:30:24 +0200

    sign-up form skeleton created

--- water-my-plants/src/Components/sign-up.js
+++ water-my-plants/src/Components/sign-up.js
@@ -1,7 +1,74 @@
+import React, { useState, useHistory, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
 
 const SignUpForm = () => {
-    return(
-        <h1>SIGN UP</h1>
-    )
+  // STATES
+  const initialFormValue = {
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const [formValue, setFormValue] = useState(initialFormValue);
+
+  //EVENT HANDLERS
+  const changeHandler = (event) => {
+    setFormValue({ ...formValue, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValue);
+  };
+
+
+  return (
+    <div>
+      <h1>SIGN UP</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Name"
+          onChange={changeHandler}
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onChange={changeHandler}
+        />
+        <input
+          type="tel"
+          name="phone"
+          id="phone"
+          placeholder="Phone Number"
+          onChange={changeHandler}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          onChange={changeHandler}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          placeholder="confirmPassword"
+          onChange={changeHandler}
+        />
+        <button>Sign Up</button>
+      </form>
+      <p>Already have an account?<span>Sign in.</span></p>
+    </div>
+  );
 }
 export default SignUpForm
