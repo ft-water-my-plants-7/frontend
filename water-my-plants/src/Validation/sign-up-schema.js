d58eba954eb7fb7824565b87b6cb2528285bd50e
adampatt <adampattison08@gmail.com>
2021-05-24 23:23:26 +0200

    changes to the phone requirement schema test

--- water-my-plants/src/Validation/sign-up-schema.js
+++ water-my-plants/src/Validation/sign-up-schema.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const SignUpSchema = yup.object().shape({
   name: yup.string().required(),
   email: yup.string(),
-  phone: yup.number().required("Phone Number Required"),
+  phone: yup.number().min(11, "Phone Number Required").required(),
   password: yup.string().required(),
   confirmPassword: yup.string().required(),
 });
