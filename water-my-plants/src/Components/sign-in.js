67d663a01bcb4f22478f4fb78fa6ff2c37fd671d
adampatt <adampattison08@gmail.com>
2021-05-24 19:18:43 +0200

    skeleton of the form complete

--- water-my-plants/src/Components/sign-in.js
+++ water-my-plants/src/Components/sign-in.js
@@ -1,1 +1,18 @@
-import 
+import React, { useState, useHistory, useEffect } from "react";
+
+
+function SignInForm () {
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+               <input type="email" name="email" id="email" />
+               <input type="password" name="password" id="password"/>
+               <button>Log in</button>
+            </form>
+            <p>Already have an account? <span>Sign in</span></p>
+        </div>
+    )
+}
+
+export default SignInForm
