361b858903a404553ec8dd518096d952950a46e5
adampatt <adampattison08@gmail.com>
2021-05-24 19:55:12 +0200

    added changehandler to sign-in component

--- water-my-plants/src/Components/sign-in.js
+++ water-my-plants/src/Components/sign-in.js
@@ -1,13 +1,26 @@
 import React, { useState, useHistory, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
 
 function SignInForm () {
+
+    const initialFormValue = {
+        email: "",
+        password: "",
+    }
+    const [formValue, setFormValue] =useState(initialFormValue)
+
+    const changeHandler = event => {
+        setFormValue(event.target.value)
+    }
+
     return (
         <div>
             <h2>Login</h2>
             <form>
-               <input type="email" name="email" id="email" />
-               <input type="password" name="password" id="password"/>
+               <input type="email" name="email" id="email" onChange={changeHandler}/> Email
+               <input type="password" name="password" id="password" onChange={changeHandler}/> Password
                <button>Log in</button>
             </form>
             <p>Already have an account? <span>Sign in</span></p>
