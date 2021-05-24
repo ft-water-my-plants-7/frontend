fde45cad70662d412c75bb44279031985929847e
Zachary Cooremans <zcooremans@gmail.com>
2021-05-24 15:53:53 -0500

    add collection stuff

--- water-my-plants/src/Components/plant-collection.js
+++ water-my-plants/src/Components/plant-collection.js
@@ -1,12 +1,33 @@
+import React, {useState} from 'react'
+import IndividualPlant from './individual-plant';
 import NoPlant from './no-plant';
+const dummyData = [
+    {
+        id: 1,
+        name: 'Tim'
+    },
+    {
+        id: 2,
+        name: 'TimA'
+    },
+]
 const PlantCollection = () => {
+    const [plants, setPlants] = useState(dummyData)
+    console.log(plants)
     return(
         <div>
-            {/* {plants.length === 0 ? (<NoPlant /> ) : (
-                <h2>Water my Plants</h2>
-            )} */}
+            {plants.length === 0 ? (<NoPlant /> ) : (
+                <div>
+                    <h2>Water my Plants</h2>
+                    <div>
+                        {plants.map((plant) => (
+                            <IndividualPlant key={plant.id} plant={plant}/>
+                        ))}
+                    </div>
+                </div>
+                
+            )}
 
-            <NoPlant />
         </div>
     )
 }
