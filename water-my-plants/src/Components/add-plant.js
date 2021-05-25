d47033eb774cb5886cac12b79c2ee01e1721f18a
adampatt <adampattison08@gmail.com>
2021-05-25 14:03:52 +0200

    created two other hidden fields

--- water-my-plants/src/Components/add-plant.js
+++ water-my-plants/src/Components/add-plant.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const AddPlant = () => {
   const [form, setForm] = useState({
+    name: "",
+    latinName: "",
     nickname: "",
     waterFrequency: "",
     startWatering: "",
@@ -12,6 +14,8 @@ const AddPlant = () => {
   const submit = (event) => {
     event.preventDefault();
     const newPlant = {
+    name: form.name,
+    latinName: form.latinName,
       nickname: form.nickname,
       waterFrequency: form.waterFrequency,
       startWatering: form.startWatering,
@@ -71,48 +75,61 @@ const AddPlant = () => {
           </form>
         </div>
         <div className="add-plant-card-1">
-            <form onSubmit={submit}>
-              Nickname
-              <input
-                type="text"
-                name="nickname"
-                onChange={change}
-                placeholder="Plant Nickname"
-              />
-              <input 
+          <form onSubmit={submit}>
+            {/* Hidden fields */}
+            <input
+              type="hidden"
+              name="name"
+              onChange={change}
+              value={(form.name = "Hibiscus")}
+            />
+            <input
+            type="hidden"
+            name="latinName"
+            onChange={change}
+            value={form.latinName = "lorem ipsum"}
+            />
+            <input
               type="hidden"
               name="waterFrequency"
               onChange={change}
-              value={form.waterFrequency = "Once per week"}
-              />
-              Start watering
-              <select
-                type="text"
-                value={form.startDate}
-                name="startWatering"
-                onChange={change}
-              >
-                {/* Stretch to make the days start with todays day */}
-                <option> Choose a day </option>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-                <option value="Saturday">Saturday</option>
-                <option value="Sunday">Sunday</option>
-              </select>
-              Notes{" "}
-              <input
-                type="textarea"
-                name="notes"
-                onChange={change}
-                placeholder="Notes ..."
-              />
-              <button>Create card</button>
+              value={(form.waterFrequency = "Once per week")}
+            />
+            Nickname
+            <input
+              type="text"
+              name="nickname"
+              onChange={change}
+              placeholder="Plant Nickname"
+            />
+            Start watering
+            <select
+              type="text"
+              value={form.startDate}
+              name="startWatering"
+              onChange={change}
+            >
+              {/* Stretch to make the days start with todays day */}
+              <option> Choose a day </option>
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+              <option value="Saturday">Saturday</option>
+              <option value="Sunday">Sunday</option>
+            </select>
+            Notes{" "}
+            <input
+              type="textarea"
+              name="notes"
+              onChange={change}
+              placeholder="Notes ..."
+            />
+            <button>Create card</button>
           </form>
         </div>
-    </div>
+      </div>
     );
   };
 
