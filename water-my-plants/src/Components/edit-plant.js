d1bc6cbe4af85d04527101acd517d0a9eaabd37d
ferengitooth <ehitsamill@gmail.com>
2021-05-25 10:42:24 -0700

    fleshed out skeleton of edit plant

--- water-my-plants/src/Components/edit-plant.js
+++ water-my-plants/src/Components/edit-plant.js
@@ -1,7 +1,41 @@
+import React, {useState} from 'react'
+
+
+const initialFormValues = {
+    name:'',
+    notes:'',
+    schedule:'',
+}
 
 const EditPlant = () => {
+
+    //States
+    const [formValues, setFormValues] = useState(initialFormValues)
+
+    //Event handlers
+    const onChange = evt => {
+        const {name,value} = evt.target
+        setFormValues({...formValues, [name]:value})
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        console.log(formValues)
+    }
+
     return(
-        <h1>SIGN UP</h1>
+        <div>
+            <h2>Name of plant</h2>
+            <h4>latin name</h4>
+            <p>Watering Schedule</p>
+            <form>
+                <input type='text' name='name' onChange={onChange}/>Nickname
+                <input type='select' name='schedule' onChange={onChange}/>Start Watering
+                <input type='text' name='notes' onChange={onChange}/>Notes
+            </form>
+            <button>Save Changes</button>
+            <h4><span>Delete From Collection</span></h4>
+        </div>
     )
 }
 export default EditPlant
