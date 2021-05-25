530db57e35c87eec149ab8ce853a7575d21a190a
adampatt <adampattison08@gmail.com>
2021-05-25 13:55:07 +0200

    created hidden field for form input watering frequency

--- water-my-plants/src/Components/add-plant.js
+++ water-my-plants/src/Components/add-plant.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const AddPlant = () => {
   const [form, setForm] = useState({
     nickname: "",
+    waterFrequency: "",
     startWatering: "",
     notes: "",
   });
@@ -12,6 +13,7 @@ const AddPlant = () => {
     event.preventDefault();
     const newPlant = {
       nickname: form.nickname,
+      waterFrequency: form.waterFrequency,
       startWatering: form.startWatering,
       notes: form.notes,
     };
@@ -32,30 +34,85 @@ const AddPlant = () => {
 
     return (
       <div>
-        <form onSubmit={submit}>
-          Nickname
-          <input type="text" name="nickname" onChange={change} placeholder="Plant Nickname" />
-          Start watering
-          <select
-            type="text"
-            value={form.startDate}
-            name="startWatering"
-            onChange={change}
-          >
-            {/* Stretch to make the days start with todays day */}
-            <option> Choose a day </option>
-            <option value="Monday">Monday</option>
-            <option value="Tuesday">Tuesday</option>
-            <option value="Wednesday">Wednesday</option>
-            <option value="Thursday">Thursday</option>
-            <option value="Friday">Friday</option>
-            <option value="Saturday">Saturday</option>
-            <option value="Sunday">Sunday</option>
-          </select>
-          Notes <input type="textarea" name="notes" onChange={change} placeholder="Notes ..."/>
-          <button>ADD PLANT</button>
-        </form>
-      </div>
+        <div className="complete-card">
+          <form onSubmit={submit}>
+            Nickname
+            <input
+              type="text"
+              name="nickname"
+              onChange={change}
+              placeholder="Plant Nickname"
+            />
+            Start watering
+            <select
+              type="text"
+              value={form.startDate}
+              name="startWatering"
+              onChange={change}
+            >
+              {/* Stretch to make the days start with todays day */}
+              <option> Choose a day </option>
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+              <option value="Saturday">Saturday</option>
+              <option value="Sunday">Sunday</option>
+            </select>
+            Notes{" "}
+            <input
+              type="textarea"
+              name="notes"
+              onChange={change}
+              placeholder="Notes ..."
+            />
+            <button>ADD PLANT</button>
+          </form>
+        </div>
+        <div className="add-plant-card-1">
+            <form onSubmit={submit}>
+              Nickname
+              <input
+                type="text"
+                name="nickname"
+                onChange={change}
+                placeholder="Plant Nickname"
+              />
+              <input 
+              type="hidden"
+              name="waterFrequency"
+              onChange={change}
+              value={form.waterFrequency = "Once per week"}
+              />
+              Start watering
+              <select
+                type="text"
+                value={form.startDate}
+                name="startWatering"
+                onChange={change}
+              >
+                {/* Stretch to make the days start with todays day */}
+                <option> Choose a day </option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+                <option value="Sunday">Sunday</option>
+              </select>
+              Notes{" "}
+              <input
+                type="textarea"
+                name="notes"
+                onChange={change}
+                placeholder="Notes ..."
+              />
+              <button>Create card</button>
+          </form>
+        </div>
+    </div>
     );
   };
 
