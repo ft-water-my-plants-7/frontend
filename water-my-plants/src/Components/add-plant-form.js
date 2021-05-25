f2ca44d2a6c03de8f286e9b760c357cb634ddeb5
adampatt <adampattison08@gmail.com>
2021-05-25 17:30:25 +0200

    created plant form folder

--- water-my-plants/src/Components/add-plant-form.js
+++ water-my-plants/src/Components/add-plant-form.js
@@ -11,15 +11,16 @@ const AddPlantForm = () => {
     notes: "",
   });
 
+  //props used here? Is this a ternary operator if props ? use props : use default
   const submit = (event) => {
     event.preventDefault();
     const newPlant = {
-    name: form.name,
-    latinName: form.latinName,
-      nickname: form.nickname,
-      waterFrequency: form.waterFrequency,
-      startWatering: form.startWatering,
-      notes: form.notes,
+    // name: form.name,
+     latinName: form.latinName,
+       nickname: form.nickname,
+       waterFrequency: form.waterFrequency,
+       startWatering: form.startWatering,
+       notes: form.notes,
     };
     axios
       .post("https://reqres.in/api/users", newPlant)
@@ -134,3 +135,82 @@ const AddPlantForm = () => {
   };
 
   export default AddPlantForm;
+
+// import React from "react";
+
+// export default function FriendForm(props) {
+//   // THESE ARE THE **EXACT PROPS** FriendForm EXPECTS!!!
+//   const { values, update, submit, submit1 } = props;
+
+//   const onChange = (evt) => {
+//     // 🔥 STEP 6 - IMPLEMENT the change handler for our inputs and dropdown
+//     // a) pull the name of the input from the event object
+//     // b) pull the value of the input from the event object
+//     const { name, value } = evt.target;
+//     // c) use the `update` callback coming in through props
+//     update(name, value);
+//   };
+
+//   const onSubmit = (evt) => {
+//     // 🔥 STEP 7 - IMPLEMENT the submit handler
+//     // a) don't allow the browser to reload!
+//     // c) use the `submit` callback coming in through props
+//     evt.preventDefault();
+//     submit();
+//   };
+
+//   return (
+//     <form className="form container" onSubmit={onSubmit}>
+//       <div className="form-group inputs">
+//         {/* ////////// TEXT INPUTS ////////// */}
+//         {/* ////////// TEXT INPUTS ////////// */}
+//         {/* ////////// TEXT INPUTS ////////// */}
+//         <label>
+//           Username
+//           {/* 🔥 STEP 3 - Make an input of type `text` for username.
+//               Controlled inputs need `value` and `onChange` props.
+//               Inputs render what they're told - their current value comes from app state.
+//               At each keystroke, a change handler fires to change app state. */}
+//           <input
+//             type="text"
+//             name="username"
+//             onChange={onChange}
+//             value={values.username}
+//           />
+//         </label>
+
+//         <label>
+//           Email
+//           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
+//           <input
+//             type="email"
+//             name="email"
+//             value={values.email}
+//             onChange={onChange}
+//           />
+//         </label>
+
+//         {/* ////////// DROPDOWN ////////// */}
+//         {/* ////////// DROPDOWN ////////// */}
+//         {/* ////////// DROPDOWN ////////// */}
+//         <label>
+//           Role
+//           {/* 🔥 STEP 5 - Make dropdown for role. */}
+//           <select value={values.role} name="role" onChange={onChange}>
+//             <option value="">-- Select a Role --</option>
+//             <option value="Student">Student</option>
+//             <option value="TL">Team Lead</option>
+//             <option value="Instructor">Instructor</option>
+//             <option value="Alumni">Alumni</option>
+//           </select>
+//         </label>
+
+//         <div className="submit">
+//           <button disabled={!values.username || !values.email || !values.role}>
+//             submit
+//           </button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// }
--- water-my-plants/src/Components/add-plant.js
+++ water-my-plants/src/Components/add-plant.js
@@ -2,9 +2,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AddPlantForm from "./add-plant-form"
 
-const AddPlant = () => {
+    // const formValues = {
+    //   name: "",
+    //   latinName: "",
+    //   nickname: "",
+    //   waterFrequency: "",
+    //   startWatering: "",
+    //   // notes: form.notes,
+    // };
+
+
+    const AddPlant = () => {
+    // Onsubmithandler passing down the correct fields here?
+    // 3 x form partially prefilled and one ready to fill
+
+
+    // const formSubmit = () => {
+    //   const newPlant = {
+    //     name: formValues.username,
+    //     email: formValues.email,
+    //     waterFrequency: formValues.waterFrequency
+    // }
+
  return (
      <AddPlantForm />
  )
+
 }
+    
+
+
 export default AddPlant
+
