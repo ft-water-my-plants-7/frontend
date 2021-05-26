ad7e6184ecec3847360adfa5ab198daaa5286860
adampatt <adampattison08@gmail.com>
2021-05-26 22:09:23 +0200

    form updating but throwing a 500 error we are sending the wrong data

--- water-my-plants/src/Components/edit-user-profile.js
+++ water-my-plants/src/Components/edit-user-profile.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import styled from "styled-components";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 
 
 function EditUserProfile() {
@@ -12,6 +13,8 @@ const [user, setUser] = useState({
   phone_number: ""
 });
 
+const history = useHistory();
+const { user_id } = useParams();
 
 const onChange = event => {
     setUser({
@@ -21,6 +24,25 @@ const onChange = event => {
     })
 }
 
+const handleSubmit = event => {
+    event.preventDefault()
+    const { username, phone_number } = user;
+    axiosWithAuth()
+    .put(`/api/plants/${user_id}`, { username, phone_number })
+    .then((res) => {
+        console.log(res)
+        history.push("/user")
+    })
+    .catch((err) => {
+        debugger
+        console.log("this is the error", err)
+    })
+    // This is in place so that the form knows what we're talking about.
+    console.log("this is submit", user);
+}
+
+const {  username, phone_number } = user;
+
     return (
       <div>
         <h1>Hello edit user page </h1>
@@ -28,17 +50,19 @@ const onChange = event => {
           Username
           <input
             type="text"
-            value={user.username}
+            // GET RID OF user dot here once handleSubmit is complete
+            value={username}
             name="username"
             onChange={onChange}
           />
           Phone Number
           <input
             type="text"
-            value={user.phone_number}
+            value={phone_number}
             name="phone_number"
             onChange={onChange}
           />
+          <button onClick={handleSubmit}>Submit</button>
         </form>
       </div>
     );
