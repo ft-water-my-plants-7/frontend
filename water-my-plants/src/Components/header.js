55d0d3e8c2d685b62c41325406ce111f1b75185c
adampatt <adampattison08@gmail.com>
2021-05-27 01:22:58 +0200

    added styled components theme and styled header

--- water-my-plants/src/Components/header.js
+++ water-my-plants/src/Components/header.js
@@ -1,18 +1,74 @@
 import React from "react";
 import {useHistory} from 'react-router-dom'
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.headerColor}; ;
+`;
+
+const LeftSideNav = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-content: center;
+  width: 30%;
+  margin-left: 2%;
+  span {
+    color: ${(props) => props.theme.white};
+    padding-left: 5%;
+    display: flex;
+    font-family: ${(props) => props.theme.font};
+    align-items: center;
+    font-size: 2.5rem;
+  }
+  h2 {
+    color: ${(props) => props.theme.white};
+    padding-left: 5%;
+    display: flex;
+    font-family: ${(props) => props.theme.font};
+    align-items: center;
+  }
+`;
+
+const RightSideNav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  margin-right: 2%;
+  
+  button {
+    color: ${(props) => props.theme.white};
+    border: none;
+    font-family: ${(props) => props.theme.font};
+    background-color: ${(props) => props.theme.headerColor};
+  }
+  button:hover {
+    cursor: pointer;
+  }
+`;
+
 
 const Header = () => {
   const history = useHistory();
 
   
   return (
-    <nav>
-      <h2> WMP 💦 </h2>
-      <h2>WELCOME, KAREN!</h2>
-      <button onClick={() => history.push("/user")}>User</button>
-      <button onClick={() => history.push("/collection")}>My Collection</button>
-      <h2>SIGN OUT</h2>
-    </nav>
+    <StyledNav>
+      <LeftSideNav>
+        <span>WMP 💦</span>
+        <h2>WELCOME, KAREN!</h2>
+      </LeftSideNav>
+      <RightSideNav>
+        <button onClick={() => history.push("/user")}>
+          <h2>MY PROFILE</h2>
+        </button>
+        <button onClick={() => history.push("/collection")}>
+          <h2>SIGN OUT</h2>
+        </button>
+      </RightSideNav>
+    </StyledNav>
   );
 };
 
---
+++ water-my-plants/src/theme/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default {
+  headerColor: "#224229",
+  primaryColor: "#E5E5E5",
+  secondaryColor: "crimson",
+  tertiaryColor: "green",
+  black: "black",
+  white: "white",
+  breakpointMobile: "(max-width: 550px)",
+  font: "Amatic SC",
+};
