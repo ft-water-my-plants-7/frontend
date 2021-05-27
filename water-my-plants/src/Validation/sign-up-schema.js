3eaa34f9c83782f1c03cb98bc53894eaa9c9e2b1
adampatt <adampattison08@gmail.com>
2021-05-27 17:39:23 +0200

    Signup validation created for phone number and matching passwords

--- water-my-plants/src/Validation/sign-up-schema.js
+++ water-my-plants/src/Validation/sign-up-schema.js
@@ -2,10 +2,18 @@ import * as yup from "yup";
 
 const SignUpSchema = yup.object().shape({
   username: yup.string().required(),
-  // email: yup.string(),
-  phone_number: yup.number().min(11, "Phone Number Required").required(),
+  phone_number: yup
+    .string()
+    .required("Please enter a valid US phone number.")
+    .matches(
+      /[(]?\d{3}[-.)]?[ ]?\d{3}[-.]?\d{4}\b/,
+      "Please enter a valid US phone number."
+    ),
   password: yup.string().required(),
-  confirmPassword: yup.string().required(),
+  confirmPassword: yup.string().oneOf(
+    [yup.ref("password"), null],
+    "Passwords must match"
+  ),
 });
 
 export default SignUpSchema;
